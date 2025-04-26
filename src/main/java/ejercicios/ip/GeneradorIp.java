2d5e57956abc1b5ba14de003ff05bb2d96c02704
Javier <jrenzullis@gmail.com>
2025-04-26 13:12:32 +0200

    Clase modulo creada

--- src/main/java/ejercicios/ip/GeneradorIp.java
+++ src/main/java/ejercicios/ip/GeneradorIp.java
@@ -1,5 +1,24 @@
 package ejercicios.ip;
 
+import java.util.Random;
+
 public class GeneradorIp {
+	 private static Random rand = new Random();
+
+	    public static int generarNumero(int minimo, int maximo) {
+	        return rand.nextInt((maximo - minimo + 1)) + minimo;
+	    }
+
+	    public static String generarIPV4() {
+	        int[] ip = new int[4];
+
+	        ip[0] = generarNumero(1, 254);
+	        ip[1] = generarNumero(0, 254);
+	        ip[2] = generarNumero(0, 254);
+	        ip[3] = generarNumero(1, 254);
+
+	        return ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3];
+	    }
+	
 
 }
--- src/test/java/ejercicios/ip/GeneradorIpTest.java
+++ src/test/java/ejercicios/ip/GeneradorIpTest.java
@@ -1,5 +1,32 @@
 package ejercicios.ip;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class GeneradorIpTest {
 
+    @Test
+    public void testGenerarNumero() {
+        for (int i = 0; i < 1000; i++) {
+            int num = GeneradorIp.generarNumero(0, 254);
+            assertTrue(num >= 0 && num <= 254);
+        }
+    }
+
+    @Test
+    public void testGenerarIPV4() {
+        for (int i = 0; i < 1000; i++) {
+            String ip = GeneradorIp.generarIPV4();
+            String[] partes = ip.split("\\.");
+
+            assertEquals(4, partes.length);
+            assertNotEquals("0", partes[0]);
+            assertNotEquals("0", partes[3]);
+
+            for (String parte : partes) {
+                int n = Integer.parseInt(parte);
+                assertTrue(n >= 0 && n <= 254);
+            }
+        }
+    }
 }
